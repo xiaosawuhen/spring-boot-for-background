18188274e4046cd0bbf74b58e8666ef8b31d5a19
nannan.c.wang@accenture.com <nannan.c.wang@accenture.com>
2017-08-02 22:29:39 +0800

    commit by wnn for test conflict

--- src/main/java/com/lxzl/Runner.java
+++ src/main/java/com/lxzl/Runner.java
@@ -10,5 +10,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Runner {
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
-    }
+    }  
 }
